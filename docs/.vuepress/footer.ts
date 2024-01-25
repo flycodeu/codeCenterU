f0229e95c9997b75b106e5bb36d1f5ed496bdcf5
fly <1785613048@qq.com>
2024-01-25 14:10:01 +0800

    添加备案号

--- docs/.vuepress/footer.ts
+++ docs/.vuepress/footer.ts
@@ -7,5 +7,9 @@ export default {
       label: "站长 - 程序员飞云",
       // icon: "/icon/user.svg",
       href: "https://flycode.icu",
-    }]
+    }],
+  copyright: {
+    href: "https://beian.miit.gov.cn/",
+    name: "苏ICP备2023015652号",
+  },
 };
