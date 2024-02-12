4d45ee0d6e0d0c6e7418a66a2ee1a83dd71e3e55
flycodeu <1785613048@qq.com>
2024-02-12 14:40:48 +0800

    修复导航

--- docs/.vuepress/sidebars/knowledgeShareSideBar.ts
+++ docs/.vuepress/sidebars/knowledgeShareSideBar.ts
@@ -4,14 +4,19 @@ export default [
         title: "知识碎片",
         collapsable: false,
         children: [
+            "性能优化.md",
+            "存储优化.md",
             "Spring-Validation注解实现校验.md",
             "FreeMarker模板引擎.md",
             "复制文件.md",
             "Java命令行开发.md",
             "对象存储使用.md",
             "自定义Starter.md",
-            "性能优化.md",
-            "存储优化.md"
+            "支付宝沙箱.md",
+            "简单Java爬虫.md",
+            "RabbitMq配置及使用.md",
+            "SpringBoot发送简单邮件.md",
+            "SpringBoot结合七牛云.md"
         ],
     },
 ];
